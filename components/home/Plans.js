4d1189abdb101b7d40bb08af5e7582987be56051
GaziRiad <ouhibimustapha36@gmail.com>
2024-09-17 02:54:45 +0100

    Updated lamp

--- components/home/Plans.js
+++ components/home/Plans.js
@@ -32,7 +32,7 @@ const PLANS = [
 
 function Plans() {
   return (
-    <section className="mx-auto max-w-[1200px] px-5">
+    <section className="relative z-10 mx-auto max-w-[1200px] px-5">
       <h2 className="mb-5 bg-secondary-heading-gradient bg-clip-text text-center text-4xl font-bold text-transparent lg:mb-10 lg:text-[52px]">
         Plans & Features
       </h2>
